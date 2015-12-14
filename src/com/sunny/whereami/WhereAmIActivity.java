d5d39d5eb54a1516e2ff7c9f1b2408e38a8a6f45
Zhou Zejin <zjzhou1@chinaums.com>
2015-12-14 12:22:29 +0800

    对Where Am I示例进行地理编码。

--- src/com/sunny/whereami/WhereAmIActivity.java
+++ src/com/sunny/whereami/WhereAmIActivity.java
@@ -1,8 +1,14 @@
 package com.sunny.whereami;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
 import android.app.Activity;
 import android.content.Context;
+import android.location.Address;
 import android.location.Criteria;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -43,13 +49,39 @@ public class WhereAmIActivity extends Activity {
 		myLocationText = (TextView) findViewById(R.id.myLocationText);
 		
 		String latLongString = "No location found";
+		String addressString = "No address found";
+		
 		if (location != null) {
 			double lat = location.getLatitude();
 			double lng = location.getLongitude();
 			latLongString = "Lat:" + lat + "\nLong:" + lng;
+			
+			double latitude = location.getLatitude();
+			double longitude = location.getLongitude();
+			Geocoder gc = new Geocoder(this, Locale.getDefault());
+			
+			try {
+				List<Address> addresses = gc.getFromLocation(latitude, longitude, 1);
+				StringBuilder sb = new StringBuilder();
+				if (addresses.size() > 0) {
+					Address address = addresses.get(0);
+					
+					for (int i = 0; i < address.getMaxAddressLineIndex(); i++) {
+						sb.append(address.getAddressLine(i)).append("\n");
+						sb.append(address.getPostalCode()).append("\n");
+						sb.append(address.getCountryName());
+					}
+					
+					addressString = sb.toString();
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 		
-		myLocationText.setText("Your Current Position is:\n" + latLongString);
+		myLocationText.setText("Your Current Position is:\n" + 
+				latLongString + "\n\n" + addressString);
 	}
 	
 	private final LocationListener locationListener = new LocationListener() {
